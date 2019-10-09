e62949f2d6863b7de88c275dab14147a5451fe1a
Chase McManning <cmcmanning@gmail.com>
2019-10-09 18:11:22 -0400

    Fix copy operations deleting source edges

--- Assets/Graph2/Editor/CopyPasteGraph.cs
+++ Assets/Graph2/Editor/CopyPasteGraph.cs
@@ -15,17 +15,17 @@ namespace Graph2
         [Serializable]
         public class SerializedEdge
         {
-            public int inputID;
+            public string inputGuid;
             public string inputPortName;
 
-            public int outputID;
+            public string outputGuid;
             public string outputPortName;
         }
 
         [Serializable]
         public class SerializedNode
         {
-            public int id;
+            public string guid;
             public string name;
             public string type;
             public string JSON;
@@ -43,20 +43,14 @@ namespace Graph2
         public static string Serialize(IEnumerable<GraphElement> elements)
         {
             var graph = CreateInstance<CopyPasteGraph>();
-
-            // Problem: If we serialize SO's it just serializes references.
-            // This is fine for copy/paste or duplicate, but breaks for cut
-            // since the referenced entity no longer exists. 
-
-            // Either we copy the referenced into some sort of temp storage,
-            // or we serialize to JSON (which doesn't seem natively supported
-            // for ScriptableObjects -> JSON)
-
+            
             foreach (var element in elements)
             {
                 if (element is NodeView)
                 {
-                    var node = (element as NodeView).NodeData;
+                    var src = (element as NodeView).NodeData;
+                    var node = Instantiate(src);
+                    node.name = src.name;
                     
                     // Convert connections to something that can be serialized
                     foreach (var port in node.inputs)
@@ -65,28 +59,23 @@ namespace Graph2
                         {
                             graph.m_SerializedEdges.Add(new SerializedEdge()
                             {
-                                inputID = node.GetInstanceID(),
+                                inputGuid = node.guid,
                                 inputPortName = port.portName,
-                                outputID = conn.node.GetInstanceID(),
+                                outputGuid = conn.node.guid,
                                 outputPortName = conn.portName
                             });
                         }
 
                         port.connections.Clear();
                     }
-
-                    // TODO: This copy/paste is deleting connections from the source element.
-                    // But I can't instantiate() because I need the original's InstanceID().
-                    // I can either copy connections to a temp storage, serialize, and then
-                    // copy back or ... something else?
-
+                    
                     foreach (var port in node.outputs)
                     {
                         port.connections.Clear();
                     }
-
+                    
                     graph.m_SerializedNodes.Add(new SerializedNode() {
-                        id = node.GetInstanceID(),
+                        guid = node.guid,
                         name = node.name,
                         type = node.GetType().FullName,
                         JSON = JsonUtility.ToJson(node)
@@ -119,24 +108,24 @@ namespace Graph2
             
             var graph = CreateInstance<Graph>();
 
-            var nodeMap = new Dictionary<int, AbstractNode>();
+            var nodeMap = new Dictionary<string, AbstractNode>();
             foreach (var node in cpGraph.m_SerializedNodes)
             {
                 var instance = graph.AddNode(node.type);
                 JsonUtility.FromJsonOverwrite(node.JSON, instance);
                 instance.name = node.name;
                 
-                nodeMap.Add(node.id, instance);
+                nodeMap.Add(node.guid, instance);
             }
             
             // Re-associate edges to the new node instances
             foreach (var edge in cpGraph.m_SerializedEdges)
             {
-                if (nodeMap.ContainsKey(edge.inputID) && 
-                    nodeMap.ContainsKey(edge.outputID)
+                if (nodeMap.ContainsKey(edge.inputGuid) && 
+                    nodeMap.ContainsKey(edge.outputGuid)
                 ) {
-                    var inPort = nodeMap[edge.inputID].GetInputPort(edge.inputPortName);
-                    inPort.Connect(nodeMap[edge.outputID], edge.outputPortName);
+                    var inPort = nodeMap[edge.inputGuid].GetInputPort(edge.inputPortName);
+                    inPort.Connect(nodeMap[edge.outputGuid], edge.outputPortName);
                 }
             }
 
