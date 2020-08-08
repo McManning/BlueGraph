3e33942bfeb1397d521626f74d88ff337cc152ee
Chase McManning <mcmanning.1@osu.edu>
2020-08-08 01:29:08 -0400

    Add comments to ICanDirty

--- Editor/ICanDirty.cs
+++ Editor/ICanDirty.cs
@@ -1,9 +1,19 @@
 ﻿
 namespace BlueGraph.Editor
 {
+    /// <summary>
+    /// Objects that can be dirtied by canvas changes and later updated in response. 
+    /// </summary>
     public interface ICanDirty
     {
+        /// <summary>
+        /// Called when the Canvas dirties this object
+        /// </summary>
         void OnDirty();
+
+        /// <summary>
+        /// Called when the canvas iterates through dirtied objects during an update loop
+        /// </summary>
         void OnUpdate();
     }
 }
