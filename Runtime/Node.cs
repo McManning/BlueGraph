e8989556971b794a7af9902d1800d0935f273d27
juliocp <juliocp.next@gmail.com>
2020-09-25 10:24:08 -0300

    Node.cs overloading functions
    - Added GetInputValue overloading with Port parameter
    - Added GetInputValues overloading with Port parameter
    - Added GetOutputValues overloading with Port parameter

--- Runtime/Node.cs
+++ Runtime/Node.cs
@@ -9,33 +9,33 @@ namespace BlueGraph
     {
         public event Action OnValidateEvent;
         public event Action OnErrorEvent;
-        
+
         [SerializeField] private string id;
 
-        public string ID 
+        public string ID
         {
             get { return id; }
             set { id = value; }
         }
-        
+
         [SerializeField] private string name;
 
-        public string Name 
+        public string Name
         {
             get { return name; }
             set { name = value; }
         }
-        
+
         [SerializeField] private Graph graph;
 
-        public Graph Graph 
-        { 
+        public Graph Graph
+        {
             get { return graph; }
             internal set { graph = value; }
         }
 
         [SerializeField] private Vector2 position;
-       
+
         /// <summary>
         /// Where this node is located on the Graph in CanvasView
         /// </summary>
@@ -46,15 +46,17 @@ namespace BlueGraph
         }
 
         [SerializeField] private List<Port> ports;
-        
+
         [NonSerialized] private string error;
 
         /// <summary>
         /// Error information associated with this node
         /// </summary>
-        public string Error { 
-            get {
-                return error; 
+        public string Error
+        {
+            get
+            {
+                return error;
             }
             set
             {
@@ -67,9 +69,9 @@ namespace BlueGraph
         /// <summary>
         /// Accessor for ports and their connections to/from this node.
         /// </summary>
-        public IList<Port> Ports 
-        { 
-            get { return ports.AsReadOnly(); } 
+        public IList<Port> Ports
+        {
+            get { return ports.AsReadOnly(); }
         }
 
         public Node()
@@ -112,12 +114,12 @@ namespace BlueGraph
         /// or when the node is added to the graph via <c>Graph.AddNode</c>
         /// </summary>
         public virtual void OnEnable() { }
-        
+
         public void Disable()
         {
             OnDisable();
         }
-        
+
         /// <summary>
         /// Called when the Graph's ScriptableObject gets the OnDisable message
         /// or when the node is removed from the graph via <c>Graph.RemoveNode</c>
@@ -133,7 +135,7 @@ namespace BlueGraph
                 port.Node = this;
                 port.OnValidate();
             }
-            
+
             OnValidate();
             OnValidateEvent?.Invoke();
         }
@@ -157,18 +159,18 @@ namespace BlueGraph
         /// 
         /// <c>Node.Graph</c> property is still valid during this call. 
         /// </summary>
-        public virtual void OnRemovedFromGraph() { } 
+        public virtual void OnRemovedFromGraph() { }
 
         /// <summary>
         /// Called when the <c>Error</c> property is modified.
         /// </summary>
-        public virtual void OnError() { } 
+        public virtual void OnError() { }
 
         /// <summary>
         /// Resolve the return value associated with the given port. 
         /// </summary>
         public abstract object OnRequestValue(Port port);
-        
+
         /// <summary>
         /// Get either an input or output port by name.
         /// </summary>
@@ -176,7 +178,7 @@ namespace BlueGraph
         {
             return ports.Find((port) => port.Name == name);
         }
-        
+
         /// <summary>
         /// Add a new port to this node.
         /// </summary>
@@ -193,7 +195,7 @@ namespace BlueGraph
             ports.Add(port);
             port.Node = this;
         }
-        
+
         /// <summary>
         /// Remove an existing port from this node.
         /// </summary>
@@ -204,7 +206,7 @@ namespace BlueGraph
 
             ports.Remove(port);
         }
-        
+
         /// <summary>
         /// Safely remove every edge going in and out of this node.
         /// </summary>
@@ -224,13 +226,30 @@ namespace BlueGraph
         public T GetInputValue<T>(string portName, T defaultValue = default)
         {
             var port = GetPort(portName);
-            if (port == null || port.Direction == PortDirection.Output)
+            return GetInputValue<T>(port, defaultValue);
+        }
+
+        /// <summary>
+        /// Get the value returned by an output port connected to the given port.
+        /// 
+        /// This will return <c>defaultValue</c> if the port is disconnected.
+        /// </summary>
+        public T GetInputValue<T>(Port port, T defaultValue = default)
+        {
+            if (port == null)
             {
                 throw new ArgumentException(
-                    $"[{Name}] No input port named `{portName}`"
+                    $"<b>[{Name}]</b> Null input port parameter"
                 );
             }
-            
+
+            if (port.Direction == PortDirection.Output)
+            {
+                throw new ArgumentException(
+                    $"<b>[{Name}]</b> Wrong input port direction `{port.Name}`"
+                );
+            }
+
             return port.GetValue(defaultValue);
         }
 
@@ -243,13 +262,31 @@ namespace BlueGraph
         public IEnumerable<T> GetInputValues<T>(string portName)
         {
             var port = GetPort(portName);
-            if (port == null || port.Direction == PortDirection.Output)
+            return GetInputValues<T>(port);
+        }
+
+        /// <summary>
+        /// Get a list of output values for all output ports connected
+        /// to the given input port. 
+        /// 
+        /// This will return an empty list if the port is disconnected.
+        /// </summary>
+        public IEnumerable<T> GetInputValues<T>(Port port)
+        {
+            if (port == null)
             {
                 throw new ArgumentException(
-                    $"<b>[{Name}]</b> No input port named `{portName}`"
+                    $"<b>[{Name}]</b> Null input port parameter"
                 );
             }
-            
+
+            if (port.Direction == PortDirection.Output)
+            {
+                throw new ArgumentException(
+                    $"<b>[{Name}]</b> Wrong input port direction `{port.Name}`"
+                );
+            }
+
             return port.GetValues<T>();
         }
 
@@ -259,16 +296,30 @@ namespace BlueGraph
         public T GetOutputValue<T>(string portName)
         {
             var port = GetPort(portName);
-            if (port == null || port.Direction == PortDirection.Input)
+            return GetOutputValue<T>(port);
+        }
+        /// <summary>
+        /// Get the calculated value of a given output port.
+        /// </summary>
+        public T GetOutputValue<T>(Port port)
+        {
+            if (port == null)
             {
                 throw new ArgumentException(
-                    $"<b>[{Name}]</b> No output port named `{portName}`"
+                    $"<b>[{Name}]</b> Null output port parameter"
+                );
+            }
+
+            if (port.Direction == PortDirection.Input)
+            {
+                throw new ArgumentException(
+                    $"<b>[{Name}]</b> Wrong output port direction `{port.Name}`"
                 );
             }
 
             return port.GetValue(default(T));
         }
-        
+
         public override string ToString()
         {
             return $"{GetType()}({Name}, {ID})";
